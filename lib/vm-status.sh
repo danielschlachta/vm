6b863e67efa3d77bebebc6dacef92ba63803ef2d
Daniel Schlachta <daniel.schlachta@gmail.com>
2023-09-03 19:22:53 +0300

    Fixed glitch in vm status

--- lib/vm-status.sh
+++ lib/vm-status.sh
@@ -49,12 +49,14 @@ if [ "$H_SSH" != "ok" -a "$NOSSH" = "0" ]; then
     exit 1
 fi
 
-vm_echo_if_verbose Guest ssh: $H_SSH
+vm_echo_if_verbose Guest \'$VM_NET_GUEST\' ssh service: $H_SSH
 
 if [ "$H_SSH" != "ok" ]; then exit 0; fi
 
 H_SUSP=`vm_get_suspend_method`
 
-test -z $H_SUSP && H_SUSP='<not available>'
-
-vm_echo_if_verbose Guest suspend: $H_SUSP
+if [ "$H_SUSP" != "" ]; then
+    vm_echo_if_verbose Guest suspend: $H_SUSP
+else
+    vm_echo_if_verbose Guest suspend not available
+fi
