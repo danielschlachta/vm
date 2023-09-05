55c1d35e64d14fba5795bae193b38a37555dd83e
Daniel Schlachta <daniel.schlachta@gmail.com>
2023-09-05 23:38:41 +0300

    Cosmetics

--- lib/vm-archive.sh
+++ lib/vm-archive.sh
@@ -6,7 +6,8 @@ function save_backing()
     TSTAMP=`date -r $VM_SNAPSHOT_BACKING_FILENAME "+%Y%m%d%H%M%S"`
     TARGET=$VM_ARCHIVE/$VM_MACHINE_NAME-$VM_SNAPSHOT_BACKINGNAME-$TSTAMP.$VM_FMT
 
-    if [ "$VERBOSE" = "1" ]; then vm_echo Saving backing file \'$VM_SNAPSHOT_BACKING_FILENAME\'; else Q=-q; fi
+    vm_echo_if_verbose Saving backing file \'$VM_SNAPSHOT_BACKING_FILENAME\'
+    if [ "$VERBOSE" = "0" ]; then Q=-q; fi
     pv $Q $VM_SNAPSHOT_BACKING_FILENAME > $TARGET
 }
 
@@ -16,7 +17,8 @@ function compress_backing()
     TARGET=$VM_ARCHIVE/$VM_MACHINE_NAME-$VM_SNAPSHOT_BACKINGNAME-$TSTAMP.$VM_FMT
 
     if [ "$COMPRESS" = "1" ]; then
-        if [ "$VERBOSE" = "1" ]; then vm_echo Compressing archived file using $COMPRESSOR; V=-v; fi
+        vm_echo_if_verbose Compressing archived file using $COMPRESSOR
+        if [ "$VERBOSE" = "1" ]; then V=-v; fi
         $COMPRESSOR -f $V $TARGET;
     fi
 }
@@ -29,23 +31,25 @@ if [ "$CREATE" = "1" ]; then
 
     vm_check_running && vm_die virtual machine is running
 
-    if [ "$VERBOSE" = "1" ]; then vm_echo Creating archive directory \'$VM_ARCHIVE\'; V=-v; else Q=-q; fi
-    mkdir -p $VM_ARCHIVE
+    vm_echo_if_verbose Creating archive directory \'$VM_ARCHIVE\'
+    mkdir -p $VM_ARCHIVE || vm_die creating archive directory \'$VM_ARCHIVE\' failed
+
+    if [ "$VERBOSE" = "1" ]; then V=-v; else Q=-q; fi
 
     if [ "$VM" != "" ]; then
-        if [ "$VERBOSE" = "1" ]; then vm_echo Deleting snapshot \'$VM\'; fi
+        vm_echo_if_verbose Deleting snapshot \'$VM\'
         qemu-img snapshot $Q -d $VM $VM_SNAPSHOT_BACKING_FILENAME
     fi
 
-    if [ "$VERBOSE" = "1" ]; then vm_echo Committing changes to base; else Q=-q; fi
+    vm_echo_if_verbose Committing changes to base
     qemu-img commit $Q $VM_SNAPSHOT_BACKING_FILENAME || vm_die qemu-img commit failed
 
     VM_ARCHIVE=$VM_MACHINE_NAME-`date -r $VM_SNAPSHOT_BASE_FILENAME "+%Y%m%d%H%M%S"`
 
-    if [ "$VERBOSE" = "1" ]; then vm_echo Creating new backing file; fi
+    vm_echo_if_verbose Creating new backing file
     qemu-img create $Q -f $VM_FMT -b $VM_SNAPSHOT_BASE_FILENAME -F $VM_FMT $VM_SNAPSHOT_BACKING_FILENAME
 
-    if [ "$VERBOSE" = "1" ]; then vm_echo Securing base file \'$VM_SNAPSHOT_BASE_FILENAME\'; fi
+    vm_echo_if_verbose Saving base file \'$VM_SNAPSHOT_BASE_FILENAME\'
     pv $Q $VM_SNAPSHOT_BASE_FILENAME > $VM_ARCHIVE/$VM_SNAPSHOT_BASE_FILENAME
     if [ "$COMPRESS" = "1" ]; then $COMPRESSOR $V $VM_ARCHIVE/$VM_SNAPSHOT_BASE_FILENAME; fi
 
@@ -54,21 +58,22 @@ if [ "$CREATE" = "1" ]; then
 else
     test -d $VM_ARCHIVE || vm_die archive directory \'$VM_ARCHIVE\' not found, use --create
     test -f $VM_SNAPSHOT_BACKING_FILENAME || vm_die backing file \'$VM_SNAPSHOT_BACKING_FILENAME\' not found
-
     vm_check_running || vm_die virtual machine not running
 
-    if [ "$VERBOSE" = "1" ]; then vm_echo Stopping virtual machine; fi
+    vm_echo_if_verbose Using archive directory \'$VM_ARCHIVE\'
+
+    vm_echo_if_verbose Stopping virtual machine
     vm cmd stop
 
     if [ "$VM" != "" ]; then
-        if [ "$VERBOSE" = "1" ]; then vm_echo Saving snapshot \'$VM\' ; fi
+        vm_echo_if_verbose Saving snapshot \'$VM\'
         vm cmd savevm $VM
         sleep 5
     fi
 
     save_backing
 
-    if [ "$VERBOSE" = "1" ]; then vm_echo re-starting virtual machine; fi
+    vm_echo_if_verbose Resuming virtual machine
     vm cmd cont
 
     compress_backing
